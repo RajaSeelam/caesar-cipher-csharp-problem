6073edae77da92f0c96962a3b769154c447509d1
unknown <rajarao.seelam@cgi.com>
2019-08-29 17:04:39 +0530

    assignment2

--- CaesarCipher/CaesarCipher.cs
+++ CaesarCipher/CaesarCipher.cs
@@ -5,11 +5,39 @@ namespace CaesarCipher
 {
     public static class RotationalCipher
     {
+      
         public static string Rotate(string text, int shiftKey)
         {
-            throw new NotImplementedException("You need to implement this function.");
+            string ans = "";
+            for(int i=0;i<text.Length;i++)
+            {
+                char cipher;
+
+                if(char.IsLetter(text[i]))
+                {
+                    if (char.IsUpper(text[i]))
+                    {
+                        cipher = (char)(((text[i] - 65 + shiftKey) % 26) + 65);
+                    }
+                    else
+                    {
+                        cipher = (char)(((text[i] - 97 + shiftKey) % 26) + 97);
+                    }
+
+                    ans = ans + cipher;
+
+                }
+
+                else
+                {
+                    ans = ans + text[i];
+                }
+                
+             }
+            return ans;
         }
         public static void Main(string[] args){
+            Rotate("raja", 3);
             
         }
     }
